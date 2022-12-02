8d0c955af058f8f6a0afb6430250a366fd681830
Deokhaeng <boyohaeng@gmail.com>
2022-12-02 23:32:55 +0900

    Feat:add AlertModal component

---
+++ PracticeStudio/src/components/AlertModal.tsx
@@ -0,0 +1,141 @@
+import React from 'react';
+import { Modal } from 'react-native';
+import styled from 'styled-components/native';
+// import { Divider, Spacer, Typo } from '@components';
+import { AlertModalPropsType } from '~types/otherTypes';
+import Divider from './Divider';
+import Spacer from './Spacer';
+import Typo from './Typo';
+
+const Alert = {
+  Background: styled.TouchableOpacity(({ theme }) => ({
+    backgroundColor: theme.colors.BLACK,
+    width: '100%',
+    height: '100%',
+    opacity: 0.3,
+  })),
+  Container: styled.View({
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: '15%',
+  }),
+  Box: styled.View<Partial<AlertModalPropsType>>(({ theme, save, deletes }) => ({
+    backgroundColor: theme.colors.WHITE,
+    width: '100%',
+    height: save || deletes ? 163 : 139,
+    borderRadius: 6,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  })),
+  TopBox: styled.View({
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }),
+  BottomBox: styled.View(({ theme }) => ({
+    flexDirection: 'row',
+    backgroundColor: theme.colors.BLACK,
+    alignItems: 'center',
+    width: '100%',
+    height: 54,
+    borderBottomLeftRadius: 6,
+    borderBottomRightRadius: 6,
+  })),
+  Button: styled.TouchableOpacity({
+    flex: 1,
+    alignItems: 'center',
+  }),
+  Typo: styled(Typo.Normal_4)<Partial<AlertModalPropsType>>(({ theme, cancel, deletes, check, save }) => ({
+    fontWeight: 600,
+    color: cancel
+      ? theme.colors.WHITE
+      : deletes
+      ? theme.colors.PINK
+      : check || save
+      ? theme.colors.SKYBLUE
+      : theme.colors.TEXT_DEFAULT,
+  })),
+  SubTypo: styled(Typo.Normal_3)(({ theme }) => ({
+    color: theme.colors.MIDIUMGRAY,
+    fontWeight: 600,
+  })),
+};
+
+export default function AlertModal({
+  alertText,
+  setModalVisible,
+  modalVisible,
+  save,
+  deletes,
+  check,
+  setValue,
+  value,
+}: Partial<AlertModalPropsType>) {
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={modalVisible}
+      presentationStyle={'overFullScreen'}
+      onRequestClose={() => {
+        setModalVisible && setModalVisible(!modalVisible);
+      }}>
+      <Alert.Background
+        onPress={() => {
+          setModalVisible && setModalVisible(!modalVisible);
+        }}
+      />
+      <Alert.Container>
+        <Alert.Box save={save} deletes={deletes}>
+          <Alert.TopBox>
+            <Alert.Typo>{alertText}</Alert.Typo>
+            {save && (
+              <>
+                <Spacer height={5} />
+                <Alert.SubTypo>위의 동영상을 저장할까요?</Alert.SubTypo>
+              </>
+            )}
+            {deletes && (
+              <>
+                <Spacer height={5} />
+                <Alert.SubTypo>위의 동영상을 삭제할까요?</Alert.SubTypo>
+              </>
+            )}
+          </Alert.TopBox>
+          <Alert.BottomBox>
+            {check || save || deletes ? (
+              <>
+                <Alert.Button
+                  onPress={() => {
+                    setModalVisible && setModalVisible(!modalVisible);
+                  }}>
+                  <Alert.Typo cancel>취소</Alert.Typo>
+                </Alert.Button>
+                <Divider height />
+                <Alert.Button
+                  onPress={() => {
+                    setModalVisible && setModalVisible(!modalVisible);
+                    setValue && setValue(!value);
+                  }}>
+                  {check && <Alert.Typo check>확인</Alert.Typo>}
+                  {save && <Alert.Typo save>저장</Alert.Typo>}
+                  {deletes && <Alert.Typo deletes>삭제</Alert.Typo>}
+                </Alert.Button>
+              </>
+            ) : (
+              <Alert.Button
+                onPress={() => {
+                  setValue && setValue(!value);
+                }}>
+                <Alert.Typo check>확인</Alert.Typo>
+              </Alert.Button>
+            )}
+          </Alert.BottomBox>
+        </Alert.Box>
+      </Alert.Container>
+    </Modal>
+  );
+}
