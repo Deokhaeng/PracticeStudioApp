17805957b215218e0155f421564809a604f7d03d
Deokhaeng <boyohaeng@gmail.com>
2022-12-02 23:34:46 +0900

    Feat:add TextInputBox component

---
+++ PracticeStudio/src/components/TextInputBox.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+import { TextInputBoxProps } from '~types/otherTypes';
+
+const TextInputBox = styled.TextInput<Partial<TextInputBoxProps>>(
+  ({ borderRadius, fontWeight, fontSize, theme, maxWidth, minWidth }) => ({
+    backgroundColor: theme.colors.LIGHTGRAY,
+    borderRadius: borderRadius || 4,
+    fontWeight: fontWeight || 500,
+    fontSize: fontSize || 16,
+    paddingHorizontal: 10,
+    height: 40,
+    maxWidth: maxWidth || 300,
+    minWidth: minWidth || 100,
+  })
+);
+
+export default TextInputBox;
