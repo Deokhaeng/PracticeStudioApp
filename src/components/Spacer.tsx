26d07451d94f059cb2079728db8600127076c2e9
Deokhaeng <boyohaeng@gmail.com>
2022-12-09 23:32:20 +0900

    Fix:change folder structure

--- src/components/Spacer.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/native';
-import { SpacerPropsType } from '~types/otherTypes';
-
-const Spacer = styled.View<Partial<SpacerPropsType>>(({ height, width }) => ({
-  height: height,
-  width: width,
-}));
-
-export default Spacer;
