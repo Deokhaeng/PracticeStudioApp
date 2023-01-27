804cea6eca1151b0dc6f2b764f23d8b7b5fa59fa
Deokhaeng <boyohaeng@gmail.com>
2023-01-27 20:57:24 +0900

    fix:remove isDark util

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,6 +1,5 @@
 import isAndroid from './isAndroid';
-import isDark from './isDark';
 import stringEllipsis from './stringEllipses';
 import processAccessToken from './processAccessToken';
 
-export { isAndroid, isDark, stringEllipsis, processAccessToken };
+export { isAndroid, stringEllipsis, processAccessToken };
--- src/utils/isDark.ts
+++
@@ -1,5 +0,0 @@
-import { Appearance } from 'react-native';
-
-const isDark = Appearance.getColorScheme() === 'dark';
-
-export default isDark;
