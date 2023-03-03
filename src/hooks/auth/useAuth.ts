390323082cb47f36f44765377d33980bf8c650ea
Deokhaeng <boyohaeng@gmail.com>
2023-03-03 11:23:35 +0900

    feature:add auth api hook

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -1,17 +1,24 @@
 import { useNavigation } from '@react-navigation/native';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import processAccessToken from '@utils/processAccessToken';
 import API from 'api';
 import { AxiosError } from 'axios';
+import Token from 'token';
 import { NavigationProps } from '~types/navigationTypes';
+import { logout as kakaoLogout } from '@react-native-seoul/kakao-login';
+import { useSetRecoilState } from 'recoil';
+import { userState } from '@atoms/userState';
 
 export default function useAuth() {
   const navigation = useNavigation<NavigationProps>();
+  const setProfile = useSetRecoilState(userState);
+
   const getTpToken = useMutation(API.Auth.tpToken, {
     onError: (error: AxiosError) => {
-      console.log('error', error.response?.data);
+      console.log('getTpToken_error', error.response?.data);
     },
     onSuccess: (res) => {
+      console.log('resAccessToken', res);
       const result = processAccessToken({ accessToken: res.data.accessToken, refreshToken: res.data.refreshToken });
       if (result) {
         navigation.replace('AppMain', {});
@@ -19,5 +26,47 @@ export default function useAuth() {
     },
   });
 
-  return { getTpToken };
+  const withdraw = useMutation(API.Auth.withdraw, {
+    onError: (error: AxiosError) => {
+      console.log('withdraw_error', error);
+    },
+    onSuccess: (res) => {
+      console.log(res);
+      Token.removeToken();
+    },
+  });
+
+  const logout = () => {
+    Token.removeToken();
+    // GoogleSignin.signOut();
+    kakaoLogout();
+    setTimeout(() => {
+      navigation.replace('Auth', {});
+    }, 500);
+  };
+
+  const getProfile = useQuery(['userProfile'], () => API.Auth.getProfile(), {
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    retry: 0,
+    onError: (error: AxiosError) => {
+      console.log('getProfile_error', error);
+    },
+    onSuccess: (res) => {
+      console.log(res);
+      setProfile(res.data);
+    },
+  });
+
+  const editProfile = useMutation(API.Auth.putProfile, {
+    onError: (error: AxiosError) => {
+      console.log('editProfile_error', error);
+    },
+    onSuccess: (res) => {
+      getProfile.refetch();
+      console.log(res);
+    },
+  });
+
+  return { getTpToken, logout, withdraw, getProfile, editProfile };
 }
