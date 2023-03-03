d031233542c6260de615a13b2a600fcb933a86cc
Deokhaeng <boyohaeng@gmail.com>
2023-03-03 11:31:45 +0900

    fix:add user atom state

--- src/atoms/userState.ts
+++ src/atoms/userState.ts
@@ -1,6 +1,7 @@
+import { ProfileType } from 'api/Auth';
 import { atom } from 'recoil';
 
-export const userState = atom<{ email: string } | null>({
+export const userState = atom<ProfileType | null>({
   key: 'userState',
   default: null,
 });
