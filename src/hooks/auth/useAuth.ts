18eb02eae4d83dbc4e97733a0b74c5218f042df5
Deokhaeng <boyohaeng@gmail.com>
2023-04-01 14:56:39 +0900

    fix:Change api request method

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -6,19 +6,15 @@ import { AxiosError } from 'axios';
 import Token from 'token';
 import { NavigationProps } from '~types/navigationTypes';
 import { logout as kakaoLogout } from '@react-native-seoul/kakao-login';
-import { useSetRecoilState } from 'recoil';
-import { userState } from '@atoms/userState';
 
 export default function useAuth() {
   const navigation = useNavigation<NavigationProps>();
-  const setProfile = useSetRecoilState(userState);
 
   const getTpToken = useMutation(API.Auth.tpToken, {
     onError: (error: AxiosError) => {
       console.log('getTpToken_error', error.response?.data);
     },
     onSuccess: (res) => {
-      console.log('resAccessToken', res);
       const result = processAccessToken({ accessToken: res.data.accessToken, refreshToken: res.data.refreshToken });
       if (result) {
         navigation.replace('AppMain', {});
@@ -31,7 +27,6 @@ export default function useAuth() {
       console.log('withdraw_error', error);
     },
     onSuccess: (res) => {
-      console.log(res);
       Token.removeToken();
     },
   });
@@ -45,28 +40,5 @@ export default function useAuth() {
     }, 500);
   };
 
-  const getProfile = useQuery(['userProfile'], () => API.Auth.getProfile(), {
-    refetchOnMount: false,
-    refetchOnWindowFocus: false,
-    retry: 0,
-    onError: (error: AxiosError) => {
-      console.log('getProfile_error', error);
-    },
-    onSuccess: (res) => {
-      console.log(res);
-      setProfile(res.data);
-    },
-  });
-
-  const editProfile = useMutation(API.Auth.putProfile, {
-    onError: (error: AxiosError) => {
-      console.log('editProfile_error', error);
-    },
-    onSuccess: (res) => {
-      getProfile.refetch();
-      console.log(res);
-    },
-  });
-
-  return { getTpToken, logout, withdraw, getProfile, editProfile };
+  return { getTpToken, logout, withdraw };
 }
